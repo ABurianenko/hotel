395db10776b9058d4d52cf9299a141f937252337
ABurianenko <121970095+ABurianenko@users.noreply.github.com>
2024-12-09 15:05:49 +0000

    add js

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import './js/menu';
+import './js/booking-form';
+import './js/about-us';
+import './js/rooms-and-apartments';
+import './js/facilities';
+import './js/gallery';
+import './js/subscription-form'
