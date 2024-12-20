ba02eb1816a84f1da7469aa41b1c4d290c216b44
Uliana Seredenko <uliana1331@gmail.com>
2024-12-20 15:31:49 +0100

    375 transition +768 start

--- src/js/facilities.js
+++ src/js/facilities.js
@@ -1,2 +1,19 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
+
+const container = document.querySelector('.facilities-rooms');
+
+const accordion = new Accordion(container, {
+  // accordion options
+  showMultiple: false,
+  duration: 400,
+  onOpen: function (el) {
+    console.log('Accordion opened:', el);
+  },
+  onClose: function (el) {
+    console.log('Accordion closed:', el);
+  },
+});
+
+//first question permanently opened
+// accordion.open(0);
