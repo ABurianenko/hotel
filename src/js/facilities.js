753f5683f5d3c1d7a315d585d51da7cf084ce77d
Uliana Seredenko <uliana1331@gmail.com>
2024-12-21 14:12:38 +0100

    full opening transition

--- src/js/facilities.js
+++ src/js/facilities.js
@@ -7,12 +7,6 @@ const accordion = new Accordion(container, {
   // accordion options
   showMultiple: false,
   duration: 400,
-  onOpen: function (el) {
-    console.log('Accordion opened:', el);
-  },
-  onClose: function (el) {
-    console.log('Accordion closed:', el);
-  },
 });
 
 //first question permanently opened
